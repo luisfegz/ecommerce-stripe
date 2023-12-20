169fc2c4459bde61e3f65bd0d6a9a00007193f9c
luisfegz <87459956+luisfegz@users.noreply.github.com>
2023-12-19 22:39:17 -0500

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -8,8 +8,7 @@ import axios from "axios";
 import Table from "@/components/Table";
 import Input from "@/components/Input";
 import { RevealWrapper } from "next-reveal";
-import { useSession } from "next-auth/react";
-
+import Footer from "@/components/Footer";
 const ColumnsWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
@@ -19,18 +18,11 @@ const ColumnsWrapper = styled.div`
   gap: 40px;
   margin-top: 40px;
 `;
-
 const Box = styled.div`
   background-color: #fff;
   border-radius: 10px;
   padding: 30px;
 `;
-
-const ProductInfoCell = styled.td`
-  padding: 10px 0;
-  border-top: 1px solid rgba(0, 0, 0, 0.1);
-`;
-
 const ProductImageBox = styled.div`
   width: 70px;
   height: 100px;
@@ -54,7 +46,6 @@ const ProductImageBox = styled.div`
     }
   }
 `;
-
 const QuantityLabel = styled.span`
   padding: 0 15px;
   display: block;
@@ -63,17 +54,14 @@ const QuantityLabel = styled.span`
     padding: 0 10px;
   }
 `;
-
 const CityHolder = styled.div`
   display: flex;
   gap: 5px;
 `;
-
+const DOMICILIO_CALI = 6500;
+const DOMICILIO_OTRAS_CIUDADES = 20000;
 export default function CartPage() {
-  const {cartProducts, addProduct, removeProduct, clearCart} = useContext(CartContext);
-  {/* ESTA PARTE ES DEL ACCOUNT inicio */}
-  const {data:session} = useSession();
-  {/* ESTA PARTE ES DEL ACCOUNT final */}
+  const { cartProducts, addProduct, removeProduct, clearCart } = useContext(CartContext);
   const [products, setProducts] = useState([]);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -82,65 +70,85 @@ export default function CartPage() {
   const [streetAddress, setStreetAddress] = useState('');
   const [country, setCountry] = useState('');
   const [isSuccess, setIsSuccess] = useState(false);
-
   useEffect(() => {
     if (cartProducts.length > 0) {
-      axios.post('/api/cart', { ids: cartProducts })
-        .then(response => {
-          setProducts(response.data);
-        });
+      axios.post('/api/cart', { ids: cartProducts }).then(response => {
+        setProducts(response.data);
+      });
     } else {
       setProducts([]);
     }
   }, [cartProducts]);
-
   useEffect(() => {
     if (typeof window === 'undefined') {
       return;
     }
-    if (window?.location.href.includes('success')) {
+    if (window.location.href.includes('success')) {
       setIsSuccess(true);
       clearCart();
     }
   }, []);
-  {/* ESTA PARTE ES DEL ACCOUNT 2parte inicio */}
-  useEffect(() => {
-    if (!session) {
-      return;
-    }
-    axios.get('/api/address').then(response => {
-      setName(response.data.name);
-      setEmail(response.data.email);
-      setCity(response.data.city);
-      setPostalCode(response.data.postalCode);
-      setStreetAddress(response.data.streetAddress);
-      setCountry(response.data.country);
-    });
-  }, [session]);
-  {/* ESTA PARTE ES DEL ACCOUNT 2parte fin */}
   function moreOfThisProduct(id) {
     addProduct(id);
   }
-
   function lessOfThisProduct(id) {
     removeProduct(id);
   }
-
+  function formatPrice(value) {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
   async function goToPayment() {
     const response = await axios.post('/api/checkout', {
       name, email, city, postalCode, streetAddress, country, cartProducts,
     });
     if (response.data.url) {
-      window.location = response.data.url;
+      window.location.href = response.data.url;
     }
   }
+  function getCartText() {
+    let cartText = products.map(product => {
+      const quantity = cartProducts.filter(id => id === product._id).length;
+      return `- ${product.title} (Cantidad: ${quantity}, Precio: ${formatPrice(quantity * product.price)} COP)`;
+    }).join("%0a");
 
-  let total = 0;
-  for (const productId of cartProducts) {
-    const price = products.find(p => p._id === productId)?.price || 0;
-    total += price;
+    let shippingCost = city === 'Cali' ? DOMICILIO_CALI : DOMICILIO_OTRAS_CIUDADES;
+    cartText += `%0a%0aTotal: $${formatPrice(total + shippingCost)} COP`;
+    cartText += `%0aDomicilio: $${formatPrice(shippingCost)} COP`;
+    cartText += `%0aDomicilio: $${formatPrice(shippingCost)} COP`;
+    return cartText;
   }
 
+  async function gotowhatsapp() {
+    const nameValue = document.getElementById("name").value;
+    const cityValue = document.getElementById("city").value;
+    const postalCodeValue = document.getElementById("postalCode").value;
+    const emailValue = document.getElementById("email").value;
+    const streetAddressValue = document.getElementById("streetAddress").value;
+    const countryValue = document.getElementById("country").value;
+    const cartText = getCartText();
+    const streetAddressEncoded = encodeURIComponent(streetAddressValue);
+    const message = [
+        "Hola ¡Champion Store! Estos son mis datos de compra:",
+        `Nombre: ${nameValue}`,
+        `Ciudad: ${cityValue}`,
+        `Código postal: ${postalCodeValue}`,
+        `Email: ${emailValue}`,
+        `Dirección: ${streetAddressEncoded}`,
+        `País: ${countryValue}`,
+        "",
+        "Productos:",
+        cartText
+    ].join("%0a");
+    const url = `https://wa.me/3023639624?text=${message}`;
+  
+    window.open(url, '_blank').focus();
+  }
+  let total = 0;
+  products.forEach(product => {
+    const quantity = cartProducts.filter(id => id === product._id).length;
+    total += quantity * product.price;
+  });
+  
   if (isSuccess) {
     return (
       <>
@@ -153,10 +161,10 @@ export default function CartPage() {
             </Box>
           </ColumnsWrapper>
         </Center>
+        <Footer />
       </>
     );
   }
-
   return (
     <>
       <Header />
@@ -165,10 +173,8 @@ export default function CartPage() {
           <RevealWrapper delay={0}>
             <Box>
               <h2>Cart</h2>
-              {!cartProducts?.length && (
-                <div>Your cart is empty</div>
-              )}
-              {products?.length > 0 && (
+              {!cartProducts.length && <div>Your cart is empty</div>}
+              {products.length > 0 && (
                 <Table>
                   <thead>
                     <tr>
@@ -177,86 +183,55 @@ export default function CartPage() {
                       <th>Price</th>
                     </tr>
                   </thead>
-                  <tbody>   
-                    {products.map(product => (
-                      <tr key={product._id}>
-                        <ProductInfoCell>
-                          <ProductImageBox>
-                            <img src={product.images[0]} alt=""/>
-                          </ProductImageBox>
-                          {product.title}
-                        </ProductInfoCell>
-                        <td>
-                          <Button 
-                            onClick={() => lessOfThisProduct(product._id)}
-                          >-</Button>
-                          <QuantityLabel>
-                            {cartProducts.filter(id => id === product._id).length}
-                          </QuantityLabel>
-                          <Button 
-                            onClick={() => moreOfThisProduct(product._id)}
-                          >+</Button>
-                        </td>
-                        <td>
-                          ${cartProducts.filter(id => id === product._id).length * product.price}
-                        </td>
-                      </tr>
-                    ))}
+                  <tbody>
+                    {products.map(product => {
+                      const quantity = cartProducts.filter(id => id === product._id).length;
+                      return (
+                        <tr key={product._id}>
+                          <td>
+                            <ProductImageBox>
+                              <img src={product.images[0]} alt={product.title} />
+                            </ProductImageBox>
+                            {product.title}
+                          </td>
+                          <td>
+                            <Button onClick={() => lessOfThisProduct(product._id)}>-</Button>
+                            <QuantityLabel>{quantity}</QuantityLabel>
+                            <Button onClick={() => moreOfThisProduct(product._id)}>+</Button>
+                          </td>
+                          <td>${formatPrice(quantity * product.price)}</td>
+                        </tr>
+                      );
+                    })}
                     <tr>
                       <td></td>
                       <td></td>
-                      <td>${total}</td>
-                    </tr>           
+                      <td>Total: ${formatPrice(total)}</td>
+                    </tr>
                   </tbody>
                 </Table>
               )}
             </Box>
           </RevealWrapper>
-          {!!cartProducts?.length && (
+          {cartProducts.length > 0 && (
             <RevealWrapper delay={100}>
               <Box>
                 <h2>Order information</h2>
-                <Input type="text"
-                       placeholder="Name"
-                       value={name}
-                       name="name"
-                       onChange={ev => setName(ev.target.value)} />
-                <Input type="text"
-                       placeholder="Email"
-                       value={email}
-                       name="email"
-                       onChange={ev => setEmail(ev.target.value)}/>
+                <Input type="text" id="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
                 <CityHolder>
-                  <Input type="text"
-                         placeholder="City"
-                         value={city}
-                         name="city"
-                         onChange={ev => setCity(ev.target.value)}/>
-                  <Input type="text"
-                         placeholder="Postal Code"
-                         value={postalCode}
-                         name="postalCode"
-                         onChange={ev => setPostalCode(ev.target.value)}/>
+                  <Input type="text" id="city" placeholder="City" value={city} onChange={e => setCity(e.target.value)} />
+                  <Input type="text" id="postalCode" placeholder="Postal Code" value={postalCode} onChange={e => setPostalCode(e.target.value)} />
                 </CityHolder>
-                <Input type="text"
-                       placeholder="Street Address"
-                       value={streetAddress}
-                       name="streetAddress"
-                       onChange={ev => setStreetAddress(ev.target.value)}/>
-                <Input type="text"
-                       placeholder="Country"
-                       value={country}
-                       name="country"
-                       onChange={ev => setCountry(ev.target.value)}/>
-                <Button black block
-                        onClick={goToPayment}>
-                  Continue to payment
-                </Button>
+                <Input type="text" id="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+                <Input type="text" id="streetAddress" placeholder="Street Address" value={streetAddress} onChange={e => setStreetAddress(e.target.value)} />
+                <Input type="text" id="country" placeholder="Country" value={country} onChange={e => setCountry(e.target.value)} />
+                <Button black block onClick={gotowhatsapp}>Continue to payment</Button>
               </Box>
             </RevealWrapper>
           )}
         </ColumnsWrapper>
       </Center>
+      <Footer />
     </>
   );
 }
